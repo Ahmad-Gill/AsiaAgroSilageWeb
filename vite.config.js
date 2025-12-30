6c0a6c84392b40744c2eab5b9b15b19de211d7fe
Muhammad Ahmad Gill <m.ahmadgill01@gmail.com>
2025-12-30 21:57:21 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: '0.0.0.0',  // Allow the app to be accessible on all IP addresses
-    port: 8080,        // You can use any port you prefer
-    allowedHosts: [
-      'app-asia-agro-silage-dev0suc-fe-akc7ascghmgwc8ay.centralus-01.azurewebsites.net'  // Add your Azure host here
-    ],
-  },
+preview: {
+  host: '0.0.0.0',
+  port: 8080,
+  allowedHosts: ['*'], // Wildcard allows all hosts
+},
+
 })
